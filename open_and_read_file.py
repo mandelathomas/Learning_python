a83405c7d32b271be40cc17a62c8ea783bf32bd6
Mandela Philip Thomas <35313589+mandelathomas@users.noreply.github.com>
2024-03-16 11:14:22 -0700

    Update open_and_read_file.py

--- open_and_read_file.py
+++ open_and_read_file.py
@@ -1,9 +1,15 @@
 """
 Python allow us to read from a file using (open() and read())
-    r : read file
-    a : appending file
-    w: write to file
-    x : exclusive creation
+r	Default value. Opens a file for reading only. Raise an exception if the file does not exist.
+r+	Opens a file for reading and writing. Raise an exception if the file does not exist.
+w	Opens a file for writing only. Creates the file if it does not exist.
+w+	Opens a file for reading and writing. Creates the file if it does not exist.
+a	Opens a file for appending only. Creates the file if it does not exist.
+a+	Opens a file for appending and reading. Creates the file if it does not exist.
+x	Creates the specified file. Raise an exception if the file already exists.
+x+	Creates the specified file and Opens the file in reading and writing mode. Raise an exception if the file already exists.
+t	Default value. Opens a file in the text mode.
+b	Opens a file in the binary mode.
 """
 
 #open the file to read from
@@ -59,3 +65,71 @@ with open("python.txt", "w") as f:
 file = open("reading.txt", "r")
 file.read()
 file.seek()
+
+
+#open and read file 
+print("Read whole file!")
+file = open('json.txt', 'r')
+print(file.read())
+
+#Read a part of file
+print("Read a part of the file!")
+file = open('json.txt', 'r')
+print(file.read(30))
+
+#read file by line
+print("Read file by line!")
+file = open('json.txt', 'r')
+print(file.readlines())
+
+#Loop through the lines of the File
+
+print("Loop over file!")
+file = open('json.txt', 'r')
+for txt in file:
+  print(txt)
+
+#close file
+print("Read and close file!")
+file = open('json.txt', 'r')
+print(file.read())
+file.close()
+
+#Write to an Existing File
+'''
+"a" - append mode. to append to the end of the file.
+"w" - write mode. to overwrite any existing content of the file.
+'''
+file = open('json.txt', 'a')
+file.write("Python is a great language to learn from and be familar with if you wan to be a data scientist or engineer")
+file.close()
+
+#Create a New File
+'''
+"a" - append mode. Opens a file in append mode and creates the file if it does not exist.
+"w" - write mode. Opens a file in write mode and creates the file if it does not exist.
+"x" - create mode. Creates a file and raises exception if it already exists.
+'''
+
+file = open('python.txt', 'a')
+file = open('python.txt', 'w')
+file = open('python.txt', 'x')
+
+#delete a file
+
+import os
+os.remove('python.txt')
+
+#Check if File exist
+
+if os.path.exists('python.txt'):
+  os.remove('python.txt')
+else:
+  print("No such file with the name exist on this system. \n maybe, file was deleted or relcated")
+
+
+#Delete Folder
+import os
+os.rmdir("myfoler")
+
+
